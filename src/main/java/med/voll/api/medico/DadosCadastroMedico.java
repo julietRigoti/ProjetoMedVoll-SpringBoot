b97eb8d08a3927cafb445249f8384e54e8ba9bfe
julietRigoti <oliveirarigoti@hotmail.com>
2024-08-23 12:00:24 -0300

    :wastebasket: remove: Removendo arquivos não utilizados

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,10 +1,8 @@
 package med.voll.api.medico;
 
-import med.voll.api.pessoa.DadosPessoa;
-import med.voll.api.pessoa.endereco.DadosEndereco;
+import med.voll.api.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
-    static DadosPessoa medico = new DadosPessoa();
-    medico.setNome(nome);
-    medico.setEmail(email);
+
+
 }
