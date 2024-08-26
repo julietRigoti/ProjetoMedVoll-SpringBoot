651d05e588aca8e82cfbeadcef82e4eb31957f23
julietRigoti <oliveirarigoti@hotmail.com>
2024-08-26 12:03:00 -0300

    :recycle: refactor: Criando novos métodos

--- src/main/java/med/voll/api/controller/MedicosController.java
+++ src/main/java/med/voll/api/controller/MedicosController.java
@@ -4,7 +4,11 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -12,9 +16,22 @@ public class MedicosController {
     @Autowired
     private MedicoRepository repository;
 
+
     @PostMapping
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
+
         repository.save(new Medico(dados));
+    }
+    @GetMapping
+    public List<DadosListagemMedico> listar(Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new).toList();
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
     }
 }
